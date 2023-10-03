ce2343a3315fb52cd77420264156407f56836efc
KaiserCarlos <carlos.vilarchao710@gmail.com>
2023-10-03 12:35:18 +0200

    Hemos añadido un mensajillo: //tengo un comentario ayudaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa en Card

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,5 @@
 package edu.ncsu.monopoly;
-//updated 20222 with a comment
+//tengo un comentario ayudaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
