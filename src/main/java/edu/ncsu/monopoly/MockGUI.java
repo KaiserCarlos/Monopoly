0456d6c630c7a1540af2dc565b74126f17ae1804
1001063 <1001063@u030055.gi.ehu.es>
2023-10-06 11:07:11 +0200

    pushito

--- src/main/java/edu/ncsu/monopoly/MockGUI.java
+++ src/main/java/edu/ncsu/monopoly/MockGUI.java
@@ -11,6 +11,7 @@ public class MockGUI implements MonopolyGUI {
     }
 
     public void enablePurchaseBtn(int playerIndex) {
+    	//TODO
     }
 	public int[] getDiceRoll() {
 		int roll[] = new int[2];
